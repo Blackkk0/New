2fd38da45c8af63d37a49a36f6c432ca1e5a8f5f
Black <82063930+Blackkk0@users.noreply.github.com>
2021-08-17 22:54:15 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,16 +3,17 @@ const Discord = require ("discord.js")
 const moment = require ("moment")
 const client = new Discord.Client();
 const prefix = "";
-
+//////////Black//////////
 client.login(""); 
-
+//////////Black//////////
 client.on("ready", async () => {
   console.log(`Logged in as ${client.user.username}!`);
   client.user.setStatus("ONLINE");
-  client.user.setActivity(`Jack Sparrow`, { type: "PLAYING" });
+  client.user.setActivity(`Black Activity Online Playing`, { type: "PLAYING" });
   client.guilds.cache.forEach(g => {
     if (g.member(client.user).hasPermission("ADMINISTRATOR")) {
       g.fetchInvites().then(guildInvites => {});
     }
   });
 });
+//////////Black//////////
